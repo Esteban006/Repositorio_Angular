e38a12faade1577a8175e53c6edbe65b8d5c1a43
Esteban Balanta <juanestebanbalanta322@gmail.com>
2022-07-23 18:16:14 -0500

    Subo el archivo de enlazaqmiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloModule } from './hello/hello.module';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',
+  component: LayoutComponent,
+  children:[
+    {
+      path: '',
+      loadChildren: () => import ('./hello/hello.module').then(m => HelloModule)
+    }
+  ]
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/hello/components/inputs/inputs.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inputs',
+  templateUrl: './inputs.component.html',
+  styleUrls: ['./inputs.component.css']
+})
+export class InputsComponent implements OnInit {
+  public nombre: String;
+  public apellido: String;
+  constructor() { 
+    this.apellido = "";
+    this.nombre = "";
+   }
+
+  ngOnInit(): void {
+  }
+
+}
